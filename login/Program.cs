0272e65396bcbc4379c37d22efc971dd8929164d
srey-pich <123791278+srey-pich@users.noreply.github.com>
2024-02-03 19:19:09 +0700

    Add project files.

---
+++ login/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Login console !");
+String name = "";
+int pwd;
+
+try
+{
+    Console.Write("Enter username =");
+    name = Console.ReadLine();
+    Console.Write("Enter Password =");
+    pwd = Convert.ToInt32(Console.ReadLine());
+    if (pwd == 0 )
+    {
+        Console.WriteLine("u success !!");
+    }
+    else
+    {
+        Console.WriteLine("you fail");
+    }
+}
+catch(Exception e)
+{
+   // Console.WriteLine("Password must be interger number....");
+    Console.WriteLine(e.Message);
+}
+
